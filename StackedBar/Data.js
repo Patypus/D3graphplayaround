f37f35cfa25c6491cb8c715aa32edb7c0589bb65
Pat Howell <patrick.howell@outlook.com>
2015-11-26 22:06:28 +0000

    Started adding data for first graph

---
+++ StackedBar/Data.js
@@ -0,0 +1,86 @@
+var data = {[
+    {
+        Planet: "Mercury",
+        Atmosphere: [
+            { Gas: "Oxygen",  Percent: 42 },
+            { Gas: "Sodium",  Percent: 29 },
+			{ Gas: "Hydrogen",  Percent: 22 },
+			{ Gas: "Helium",  Percent: 6 },
+			{ Gas: "Other",  Percent: 1 },
+        ]
+    },
+	{
+        Planet: "Venus",
+        Atmosphere: [
+            { Gas: "Carbon Dioxide",  Percent: 95.5 },
+            { Gas: "Nitrogen",  Percent: 3,5 },
+			{ Gas: "Other",  Percent: 1 },
+        ]
+    },
+	{
+        Planet: "Earth",
+        Atmosphere: [
+            { Gas: "Nitrogen",  Percent: 78 },
+            { Gas: "Oxygen",  Percent: 21 },
+			{ Gas: "Other",  Percent: 1 },
+        ]
+    },
+	{
+        Planet: "Mars",
+        Atmosphere: [
+			{ Gas: "Carbon Dioxide",  Percent: 95 },
+            { Gas: "Nitrogen",  Percent: 3 },
+			{ Gas: "Argon",  Percent: 1.5 },
+			{ Gas: "Other",  Percent: 0.5 },
+        ]
+    },
+	{
+        Planet: "Jupiter",
+        Atmosphere: [
+			{ Gas: "Hydrogen",  Percent: 90 },
+            { Gas: "Helium",  Percent: 9 },
+			{ Gas: "Other",  Percent: 1 },
+        ]
+    },
+	{
+        Planet: "Saturn",
+        Atmosphere: [
+			{ Gas: "Hydrogen",  Percent: 96 },
+            { Gas: "Helium",  Percent: 3 },
+			{ Gas: "Other",  Percent: 1 },
+        ]
+    },
+	{
+        Planet: "Uranus",
+        Atmosphere: [
+			{ Gas: "Hydrogen",  Percent: 83 },
+            { Gas: "Helium",  Percent: 15 },
+			{ Gas: "Methane",  Percent: 2.5 },
+        ]
+    },
+	{
+        Planet: "Neptune",
+        Atmosphere: [
+			{ Gas: "Hydrogen",  Percent: 80 },
+            { Gas: "Helium",  Percent: 19 },
+			{ Gas: "Methane",  Percent: 1 },
+        ]
+    },
+	{
+        Planet: "Pluto",
+        Atmosphere: [
+            { Gas: "Nitrogen",  Percent: 97 },
+            { Gas: "Methane",  Percent: 2.5 },
+			{ Gas: "Carbon Monoxide",  Percent: 0.5 },
+        ]
+    },
+	{
+        Planet: "Titan",
+        Atmosphere: [
+            { Gas: "Nitrogen",  Percent: 96 },
+            { Gas: "Methane",  Percent: 3.5 },
+			{ Gas: "Hydrogen",  Percent: 0.5 },
+        ]
+    },
+	
+    ]};
