57d5e92820120652e17d7d096ad64a7e5693f3d3
Pat Howell <patrick.howell@outlook.com>
2016-01-03 22:16:04 +0000

    Added data classes for different gas types and started styling these divs.

--- StackedBar/Data.js
+++ StackedBar/Data.js
@@ -2,84 +2,84 @@ var data = [
     {
         Planet: "Mercury",
         Atmosphere: [
-            { Gas: "Oxygen",  Percent: 42 },
-            { Gas: "Sodium",  Percent: 29 },
-			{ Gas: "Hydrogen",  Percent: 22 },
-			{ Gas: "Helium",  Percent: 6 },
-			{ Gas: "Other",  Percent: 1 },
+            { Gas: "Oxygen", DataClass: "oxygen", Percent: 42 },
+            { Gas: "Sodium", DataClass: "sodium", Percent: 29 },
+			{ Gas: "Hydrogen", DataClass: "hydrogen", Percent: 22 },
+			{ Gas: "Helium", DataClass: "helium", Percent: 6 },
+			{ Gas: "Other", DataClass: "other", Percent: 1 },
         ]
     },
 	{
         Planet: "Venus",
         Atmosphere: [
-            { Gas: "Carbon Dioxide",  Percent: 95.5 },
-            { Gas: "Nitrogen",  Percent: 3.5 },
-			{ Gas: "Other",  Percent: 1 },
+            { Gas: "Carbon Dioxide", DataClass: "carbon-dioxide", Percent: 95.5 },
+            { Gas: "Nitrogen", DataClass: "nitrogen", Percent: 3.5 },
+			{ Gas: "Other", DataClass: "other", Percent: 1 },
         ]
     },
 	{
         Planet: "Earth",
         Atmosphere: [
-            { Gas: "Nitrogen",  Percent: 78 },
-            { Gas: "Oxygen",  Percent: 21 },
-			{ Gas: "Other",  Percent: 1 },
+            { Gas: "Nitrogen", DataClass: "nitrogen", Percent: 78 },
+            { Gas: "Oxygen", DataClass: "oxygen", Percent: 21 },
+			{ Gas: "Other", DataClass: "other", Percent: 1 },
         ]
     },
 	{
         Planet: "Mars",
         Atmosphere: [
-			{ Gas: "Carbon Dioxide",  Percent: 95 },
-            { Gas: "Nitrogen",  Percent: 3 },
-			{ Gas: "Argon",  Percent: 1.5 },
-			{ Gas: "Other",  Percent: 0.5 },
+			{ Gas: "Carbon Dioxide", DataClass: "carbon-dioxide", Percent: 95 },
+            { Gas: "Nitrogen", DataClass: "nitrogen", Percent: 3 },
+			{ Gas: "Argon", DataClass: "argon", Percent: 1.5 },
+			{ Gas: "Other", DataClass: "other", Percent: 0.5 },
         ]
     },
 	{
         Planet: "Jupiter",
         Atmosphere: [
-			{ Gas: "Hydrogen",  Percent: 90 },
-            { Gas: "Helium",  Percent: 9 },
-			{ Gas: "Other",  Percent: 1 },
+			{ Gas: "Hydrogen", DataClass: "hydrogen", Percent: 90 },
+            { Gas: "Helium", DataClass: "helium", Percent: 9 },
+			{ Gas: "Other", DataClass: "other", Percent: 1 },
         ]
     },
 	{
         Planet: "Saturn",
         Atmosphere: [
-			{ Gas: "Hydrogen",  Percent: 96 },
-            { Gas: "Helium",  Percent: 3 },
-			{ Gas: "Other",  Percent: 1 },
+			{ Gas: "Hydrogen", DataClass: "hydrogen", Percent: 96 },
+            { Gas: "Helium", DataClass: "helium", Percent: 3 },
+			{ Gas: "Other", DataClass: "other", Percent: 1 },
         ]
     },
 	{
         Planet: "Uranus",
         Atmosphere: [
-			{ Gas: "Hydrogen",  Percent: 83 },
-            { Gas: "Helium",  Percent: 15 },
-			{ Gas: "Methane",  Percent: 2.5 },
+			{ Gas: "Hydrogen", DataClass: "hydrogen", Percent: 83 },
+            { Gas: "Helium", DataClass: "helium", Percent: 15 },
+			{ Gas: "Methane", DataClass: "methane", Percent: 2.5 },
         ]
     },
 	{
         Planet: "Neptune",
         Atmosphere: [
-			{ Gas: "Hydrogen",  Percent: 80 },
-            { Gas: "Helium",  Percent: 19 },
-			{ Gas: "Methane",  Percent: 1 },
+			{ Gas: "Hydrogen", DataClass: "hydrogen", Percent: 80 },
+            { Gas: "Helium", DataClass: "helium", Percent: 19 },
+			{ Gas: "Methane", DataClass: "methane", Percent: 1 },
         ]
     },
 	{
         Planet: "Pluto",
         Atmosphere: [
-            { Gas: "Nitrogen",  Percent: 97 },
-            { Gas: "Methane",  Percent: 2.5 },
-			{ Gas: "Carbon Monoxide",  Percent: 0.5 },
+            { Gas: "Nitrogen", DataClass: "nitrogen", Percent: 97 },
+            { Gas: "Methane", DataClass: "methane", Percent: 2.5 },
+			{ Gas: "Carbon Monoxide", DataClass: "carbon-monoxide", Percent: 0.5 },
         ]
     },
 	{
         Planet: "Titan",
         Atmosphere: [
-            { Gas: "Nitrogen",  Percent: 96 },
-            { Gas: "Methane",  Percent: 3.5 },
-			{ Gas: "Hydrogen",  Percent: 0.5 },
+            { Gas: "Nitrogen", DataClass: "nitrogen", Percent: 96 },
+            { Gas: "Methane", DataClass: "methane", Percent: 3.5 },
+			{ Gas: "Hydrogen", DataClass: "hydrogen", Percent: 0.5 },
         ]
     }
 	];
