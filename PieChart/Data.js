9b9d27e83a349795b02533c3b6feaae417f33839
Pat Howell <patrick.howell@outlook.com>
2016-02-20 20:41:28 +0000

    Added pie chart

---
+++ PieChart/Data.js
@@ -0,0 +1,85 @@
+var data = [
+    {
+        Planet: "Mercury",
+        Atmosphere: [
+            { Gas: "Oxygen", DataClass: "oxygen", Percent: 42 },
+            { Gas: "Sodium", DataClass: "sodium", Percent: 29 },
+			{ Gas: "Hydrogen", DataClass: "hydrogen", Percent: 22 },
+			{ Gas: "Helium", DataClass: "helium", Percent: 6 },
+			{ Gas: "Other", DataClass: "other", Percent: 1 },
+        ]
+    },
+	{
+        Planet: "Venus",
+        Atmosphere: [
+            { Gas: "Carbon Dioxide", DataClass: "carbon-dioxide", Percent: 95.5 },
+            { Gas: "Nitrogen", DataClass: "nitrogen", Percent: 3.5 },
+			{ Gas: "Other", DataClass: "other", Percent: 1 },
+        ]
+    },
+	{
+        Planet: "Earth",
+        Atmosphere: [
+            { Gas: "Nitrogen", DataClass: "nitrogen", Percent: 78 },
+            { Gas: "Oxygen", DataClass: "oxygen", Percent: 21 },
+			{ Gas: "Other", DataClass: "other", Percent: 1 },
+        ]
+    },
+	{
+        Planet: "Mars",
+        Atmosphere: [
+			{ Gas: "Carbon Dioxide", DataClass: "carbon-dioxide", Percent: 95 },
+            { Gas: "Nitrogen", DataClass: "nitrogen", Percent: 3 },
+			{ Gas: "Argon", DataClass: "argon", Percent: 1.5 },
+			{ Gas: "Other", DataClass: "other", Percent: 0.5 },
+        ]
+    },
+	{
+        Planet: "Jupiter",
+        Atmosphere: [
+			{ Gas: "Hydrogen", DataClass: "hydrogen", Percent: 90 },
+            { Gas: "Helium", DataClass: "helium", Percent: 9 },
+			{ Gas: "Other", DataClass: "other", Percent: 1 },
+        ]
+    },
+	{
+        Planet: "Saturn",
+        Atmosphere: [
+			{ Gas: "Hydrogen", DataClass: "hydrogen", Percent: 96 },
+            { Gas: "Helium", DataClass: "helium", Percent: 3 },
+			{ Gas: "Other", DataClass: "other", Percent: 1 },
+        ]
+    },
+	{
+        Planet: "Uranus",
+        Atmosphere: [
+			{ Gas: "Hydrogen", DataClass: "hydrogen", Percent: 83 },
+            { Gas: "Helium", DataClass: "helium", Percent: 15 },
+			{ Gas: "Methane", DataClass: "methane", Percent: 2 },
+        ]
+    },
+	{
+        Planet: "Neptune",
+        Atmosphere: [
+			{ Gas: "Hydrogen", DataClass: "hydrogen", Percent: 80 },
+            { Gas: "Helium", DataClass: "helium", Percent: 19 },
+			{ Gas: "Methane", DataClass: "methane", Percent: 1 },
+        ]
+    },
+	{
+        Planet: "Pluto",
+        Atmosphere: [
+            { Gas: "Nitrogen", DataClass: "nitrogen", Percent: 97 },
+            { Gas: "Methane", DataClass: "methane", Percent: 2.5 },
+			{ Gas: "Carbon Monoxide", DataClass: "carbon-monoxide", Percent: 0.5 },
+        ]
+    },
+	{
+        Planet: "Titan",
+        Atmosphere: [
+            { Gas: "Nitrogen", DataClass: "nitrogen", Percent: 96 },
+            { Gas: "Methane", DataClass: "methane", Percent: 3.5 },
+			{ Gas: "Hydrogen", DataClass: "hydrogen", Percent: 0.5 },
+        ]
+    }
+	];
