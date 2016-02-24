072be64bcdb8827dd1384bea90be795f9b23beb4
Pat Howell <patrick.howell@outlook.com>
2016-02-24 21:55:19 +0000

    Added loop in to draw graph for each planet

--- PieChart/pie-graph.js
+++ PieChart/pie-graph.js
@@ -1,19 +1,31 @@
-function CreateChart(data, graphElement) {
-    var testItem = data[0];
-    var singleWidth = 300;
-    var singleHeight = 300;
-    var radius = singleHeight / 2;
+var singleWidth = 300;
+var singleHeight = 300;
+var radius = singleHeight / 2;
 
-    var title = graphElement.append("div").text(testItem.Planet);
+function CreateChart(dataCollection, graphElement) {
+    for (var index = 0; index < dataCollection.length; index++) {
+        var planet = dataCollection[index];
+        addTitle(planet, graphElement);
+        createSinglePie(planet, graphElement);
+    }
+    //TODO:
+    // - allow for each entry in the data collection 
+    // - legend on the left
+}
 
-    var chart = graphElement.append("svg:svg").data([testItem.Atmosphere])
+function addTitle(data, graphElement) {
+    graphElement.append("div").text(data.Planet);
+}
+
+function createSinglePie(planet, graphElement) {
+    var chart = graphElement.append("svg:svg").data([planet.Atmosphere])
                             .attr("width", singleWidth)
                             .attr("height", singleHeight)
                             .append("svg:g")
                             .attr("transform", "translate(" + radius + "," + radius + ")");
     var pie = d3.layout.pie().value(function (atmosphere) { return atmosphere.Percent });
     var arc = d3.svg.arc().outerRadius(radius);
-        
+
     var arcs = chart.selectAll("g.slice").data(pie).enter().append("svg:g").attr("class", "slice");
     arcs.append("svg:path")
     .attr("fill", function (sector, index) {
@@ -22,8 +34,4 @@ function CreateChart(data, graphElement) {
     .attr("d", function (sector) {
         return arc(sector);
     });
-
-    //TODO:
-    // - allow for each entry in the data collection 
-    // - legend on the left
 }
