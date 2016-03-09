8b6ce8f7bbdf0a6e4aae62de3ee1677dbede439b
Pat Howell <patrick.howell@outlook.com>
2016-03-09 22:02:28 +0000

    Updated data collection for doughnet graph

--- Doughnut/Data.js
+++ Doughnut/Data.js
@@ -2,84 +2,84 @@ var data = [
     {
         Planet: "Mercury",
         Atmosphere: [
-            { Gas: "Oxygen", DataClass: "oxygen", Percent: 42 },
-            { Gas: "Sodium", DataClass: "sodium", Percent: 29 },
-			{ Gas: "Hydrogen", DataClass: "hydrogen", Percent: 22 },
-			{ Gas: "Helium", DataClass: "helium", Percent: 6 },
-			{ Gas: "Other", DataClass: "other", Percent: 1 },
+            { Gas: "Oxygen", DataColour: "#2ECC40", Percent: 42 },
+            { Gas: "Sodium", DataColour: "#e6af4b", Percent: 29 },
+			{ Gas: "Hydrogen", DataColour: "#0074D9", Percent: 22 },
+			{ Gas: "Helium", DataColour: "#FA9339", Percent: 6 },
+			{ Gas: "Other", DataColour: "#DEDEDE", Percent: 1 },
         ]
     },
 	{
-        Planet: "Venus",
-        Atmosphere: [
-            { Gas: "Carbon Dioxide", DataClass: "carbon-dioxide", Percent: 95.5 },
-            { Gas: "Nitrogen", DataClass: "nitrogen", Percent: 3.5 },
-			{ Gas: "Other", DataClass: "other", Percent: 1 },
-        ]
-    },
+	    Planet: "Venus",
+	    Atmosphere: [
+            { Gas: "Carbon Dioxide", DataColour: "#e05038", Percent: 95.5 },
+            { Gas: "Nitrogen", DataColour: "#39CCCC", Percent: 3.5 },
+			{ Gas: "Other", DataColour: "#DEDEDE", Percent: 1 },
+	    ]
+	},
 	{
-        Planet: "Earth",
-        Atmosphere: [
-            { Gas: "Nitrogen", DataClass: "nitrogen", Percent: 78 },
-            { Gas: "Oxygen", DataClass: "oxygen", Percent: 21 },
-			{ Gas: "Other", DataClass: "other", Percent: 1 },
-        ]
-    },
+	    Planet: "Earth",
+	    Atmosphere: [
+            { Gas: "Nitrogen", DataColour: "#39CCCC", Percent: 78 },
+            { Gas: "Oxygen", DataColour: "#2ECC40", Percent: 21 },
+			{ Gas: "Other", DataColour: "#DEDEDE", Percent: 1 },
+	    ]
+	},
 	{
-        Planet: "Mars",
-        Atmosphere: [
-			{ Gas: "Carbon Dioxide", DataClass: "carbon-dioxide", Percent: 95 },
-            { Gas: "Nitrogen", DataClass: "nitrogen", Percent: 3 },
-			{ Gas: "Argon", DataClass: "argon", Percent: 1.5 },
-			{ Gas: "Other", DataClass: "other", Percent: 0.5 },
-        ]
-    },
+	    Planet: "Mars",
+	    Atmosphere: [
+			{ Gas: "Carbon Dioxide", DataColour: "#e05038", Percent: 95 },
+            { Gas: "Nitrogen", DataColour: "#39CCCC", Percent: 3 },
+			{ Gas: "Argon", DataColour: "#F76060", Percent: 1.5 },
+			{ Gas: "Other", DataColour: "#DEDEDE", Percent: 0.5 },
+	    ]
+	},
 	{
-        Planet: "Jupiter",
-        Atmosphere: [
-			{ Gas: "Hydrogen", DataClass: "hydrogen", Percent: 90 },
-            { Gas: "Helium", DataClass: "helium", Percent: 9 },
-			{ Gas: "Other", DataClass: "other", Percent: 1 },
-        ]
-    },
+	    Planet: "Jupiter",
+	    Atmosphere: [
+			{ Gas: "Hydrogen", DataColour: "#0074D9", Percent: 90 },
+            { Gas: "Helium", DataColour: "#FA9339", Percent: 9 },
+			{ Gas: "Other", DataColour: "#DEDEDE", Percent: 1 },
+	    ]
+	},
 	{
-        Planet: "Saturn",
-        Atmosphere: [
-			{ Gas: "Hydrogen", DataClass: "hydrogen", Percent: 96 },
-            { Gas: "Helium", DataClass: "helium", Percent: 3 },
-			{ Gas: "Other", DataClass: "other", Percent: 1 },
-        ]
-    },
+	    Planet: "Saturn",
+	    Atmosphere: [
+			{ Gas: "Hydrogen", DataColour: "#0074D9", Percent: 96 },
+            { Gas: "Helium", DataColour: "#FA9339", Percent: 3 },
+			{ Gas: "Other", DataColour: "#DEDEDE", Percent: 1 },
+	    ]
+	},
 	{
-        Planet: "Uranus",
-        Atmosphere: [
-			{ Gas: "Hydrogen", DataClass: "hydrogen", Percent: 83 },
-            { Gas: "Helium", DataClass: "helium", Percent: 15 },
-			{ Gas: "Methane", DataClass: "methane", Percent: 2 },
-        ]
-    },
+	    Planet: "Uranus",
+	    Atmosphere: [
+			{ Gas: "Hydrogen", DataColour: "#0074D9", Percent: 83 },
+            { Gas: "Helium", DataColour: "#FA9339", Percent: 15 },
+			{ Gas: "Methane", DataColour: "#3D9970", Percent: 2 },
+	    ]
+	},
 	{
-        Planet: "Neptune",
-        Atmosphere: [
-			{ Gas: "Hydrogen", DataClass: "hydrogen", Percent: 80 },
-            { Gas: "Helium", DataClass: "helium", Percent: 19 },
-			{ Gas: "Methane", DataClass: "methane", Percent: 1 },
-        ]
-    },
+	    Planet: "Neptune",
+	    Atmosphere: [
+			{ Gas: "Hydrogen", DataColour: "#0074D9", Percent: 80 },
+            { Gas: "Helium", DataColour: "#FA9339", Percent: 19 },
+			{ Gas: "Methane", DataColour: "#3D9970", Percent: 1 },
+	    ]
+	},
 	{
-        Planet: "Pluto",
-        Atmosphere: [
-            { Gas: "Nitrogen", DataClass: "nitrogen", Percent: 97 },
-            { Gas: "Methane", DataClass: "methane", Percent: 2.5 },
-			{ Gas: "Carbon Monoxide", DataClass: "carbon-monoxide", Percent: 0.5 },
-        ]
-    },
+	    Planet: "Pluto",
+	    Atmosphere: [
+            { Gas: "Nitrogen", DataColour: "#39CCCC", Percent: 97 },
+            { Gas: "Methane", DataColour: "#3D9970", Percent: 2.5 },
+			{ Gas: "Carbon Monoxide", DataColour: "#FFEC73", Percent: 0.5 },
+	    ]
+	},
 	{
-        Planet: "Titan",
-        Atmosphere: [
-            { Gas: "Nitrogen", DataClass: "nitrogen", Percent: 96 },
-            { Gas: "Methane", DataClass: "methane", Percent: 3.5 },
-			{ Gas: "Hydrogen", DataClass: "hydrogen", Percent: 0.5 },
-        ]
-    }
-	];
+	    Planet: "Titan",
+	    Atmosphere: [
+            { Gas: "Nitrogen", DataColour: "#39CCCC", Percent: 96 },
+            { Gas: "Methane", DataColour: "#3D9970", Percent: 3.5 },
+			{ Gas: "Hydrogen", DataColour: "#0074D9", Percent: 0.5 },
+	    ]
+	}
+];
