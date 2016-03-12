a7438aa1d0bb5bc31dd4aa342e9c7be2138a992a
Pat Howell <patrick.howell@outlook.com>
2016-03-12 21:03:41 +0000

    Fixed broken doughnut graph script

--- Doughnut/doughnut-graph.js
+++ Doughnut/doughnut-graph.js
@@ -1,33 +1,27 @@
 function CreateDoughnutGraph(data, graphElement) {
-    var firstItem = data[0].Atmosphere;
+    var firstItem = data[0];
 
     var graphSide = 300;
     var doughnutWidth = 60;
 
     var radius = Math.min(graphSide, graphSide) / 2;
+    var innerRadius = radius - doughnutWidth;
 
-
-    var graphSvg = graphElement
-                   .append("svg")
-                   .attr("height", graphSide)
-                   .attr("width", graphSide)
-                   .append("g");
-
+    var chart = graphElement.append("svg:svg").data([firstItem.Atmosphere])
+                            .attr("width", graphSide)
+                            .attr("height", graphSide)
+                            .append("svg:g")
+                            .attr("transform", "translate(" + radius + "," + radius + ")");
+    var pie = d3.layout.pie().value(function (atmosphere) { return atmosphere.Percent });
     var arc = d3.svg.arc()
-              .innerRadius(radius - doughnutWidth)
-              .outerRadius(radius)
-
-    var pie = d3.layout.pie()
-                .value(function (d) { d.Percent })
-                .sort(null);
-
-    var path = graphSvg.selectAll('path')
-          .data(pie(firstItem))
-          .enter()
-          .append('path')
-          .attr('d', arc)
-          .attr('fill', function (d, i) {
-              return "#fefefe";
-          });
-
+                    .outerRadius(radius)
+                    .innerRadius(innerRadius);
+    var arcs = chart.selectAll("g.slice").data(pie).enter().append("svg:g").attr("class", "slice");
+    arcs.append("svg:path")
+    .attr("fill", function (sector, index) {
+        return sector.data.DataColour;
+    })
+    .attr("d", function (sector) {
+        return arc(sector);
+    });
 }
