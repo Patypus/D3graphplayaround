5ef6ce9110cd7529b5e5dd3c5b0bf1f3b141113f
patrick howell <patrick.howell@outlook.com>
2017-02-09 20:34:00 +0000

    Setup data for line graph

---
+++ LineChangingSeries/Data.js
@@ -0,0 +1,314 @@
+var dailyData = 
+[
+    {
+        date: "01/01/2017",
+        bisquitsEaten: 1,
+        teasDrunk: 2
+    },
+    {
+        date: "02/01/2017",
+        bisquitsEaten: 12,
+        teasDrunk: 3
+    },
+    {
+        date: "03/01/2017",
+        bisquitsEaten: 2,
+        teasDrunk: 4
+    },
+    {
+        date: "04/01/2017",
+        bisquitsEaten: 0,
+        teasDrunk: 1
+    },
+    {
+        date: "05/01/2017",
+        bisquitsEaten: 4,
+        teasDrunk: 5
+    },
+    {
+        date: "06/01/2017",
+        bisquitsEaten: 3,
+        teasDrunk: 2
+    },
+    {
+        date: "07/01/2017",
+        bisquitsEaten: 2,
+        teasDrunk: 3
+    },
+    {
+        date: "08/01/2017",
+        bisquitsEaten: 3,
+        teasDrunk: 3
+    },
+    {
+        date: "09/01/2017",
+        bisquitsEaten: 0,
+        teasDrunk: 2
+    },
+    {
+        date: "10/01/2017",
+        bisquitsEaten: 1,
+        teasDrunk: 4
+    },
+    {
+        date: "11/01/2017",
+        bisquitsEaten: 4,
+        teasDrunk: 1
+    },
+    {
+        date: "12/01/2017",
+        bisquitsEaten: 0,
+        teasDrunk: 2
+    },
+    {
+        date: "13/01/2017",
+        bisquitsEaten: 0,
+        teasDrunk: 2
+    },
+    {
+        date: "14/01/2017",
+        bisquitsEaten: 0,
+        teasDrunk: 4
+    },
+    {
+        date: "15/01/2017",
+        bisquitsEaten: 3,
+        teasDrunk: 3
+    },
+    {
+        date: "16/01/2017",
+        bisquitsEaten: 3,
+        teasDrunk: 4
+    },
+    {
+        date: "17/01/2017",
+        bisquitsEaten: 2,
+        teasDrunk: 3
+    },
+    {
+        date: "18/01/2017",
+        bisquitsEaten: 0,
+        teasDrunk: 1
+    },
+    {
+        date: "19/01/2017",
+        bisquitsEaten: 0,
+        teasDrunk: 0
+    },
+    {
+        date: "20/01/2017",
+        bisquitsEaten: 3,
+        teasDrunk: 2
+    },
+    {
+        date: "21/01/2017",
+        bisquitsEaten: 4,
+        teasDrunk: 4
+    },
+    {
+        date: "22/01/2017",
+        bisquitsEaten: 0,
+        teasDrunk: 3
+    },
+    {
+        date: "23/01/2017",
+        bisquitsEaten: 2,
+        teasDrunk: 1
+    },
+    {
+        date: "24/01/2017",
+        bisquitsEaten: 0,
+        teasDrunk: 4
+    },
+    {
+        date: "25/01/2017",
+        bisquitsEaten: 0,
+        teasDrunk: 3
+    },
+    {
+        date: "26/01/2017",
+        bisquitsEaten: 0,
+        teasDrunk: 4
+    },
+    {
+        date: "27/01/2017",
+        bisquitsEaten: 4,
+        teasDrunk: 5
+    },
+    {
+        date: "28/01/2017",
+        bisquitsEaten: 2,
+        teasDrunk: 6
+    },
+    {
+        date: "29/01/2017",
+        bisquitsEaten: 0,
+        teasDrunk: 3
+    },
+    {
+        date: "30/01/2017",
+        bisquitsEaten: 0,
+        teasDrunk: 4
+    },
+    {
+        date: "31/01/2017",
+        bisquitsEaten: 2,
+        teasDrunk: 3
+    },
+]
+
+var cumulativeData = [
+    { 
+        date : "01/01/2017",
+        totalBiscuits: 1, 
+        totalTeas: 2
+    },
+    { 
+        date : "02/01/2017",
+        totalBiscuits: 13, 
+        totalTeas: 5
+    },
+    { 
+        date : "03/01/2017",
+        totalBiscuits: 15, 
+        totalTeas: 9
+    },
+    { 
+        date : "04/01/2017",
+        totalBiscuits: 15,
+        totalTeas: 10
+    },
+    { 
+        date : "05/01/2017",
+        totalBiscuits: 19, 
+        totalTeas: 15 
+    },
+    { 
+        date : "06/01/2017",
+        totalBiscuits: 22, 
+        totalTeas: 17 
+    },
+    { 
+        date : "07/01/2017",
+        totalBiscuits: 24, totalTeas: 20 },
+    { 
+        date : "08/01/2017",
+        totalBiscuits: 27, 
+        totalTeas: 23 
+    },
+    { 
+        date : "09/01/2017",
+        totalBiscuits: 27, 
+        totalTeas: 25 
+    },
+    { 
+        date : "10/01/2017",
+        totalBiscuits: 28, 
+        totalTeas: 29 
+    },
+    { 
+        date : "11/01/2017",
+        totalBiscuits: 32, 
+        totalTeas: 30 
+    },
+    { 
+        date : "12/01/2017",
+        totalBiscuits: 32, 
+        totalTeas: 32 
+    },
+    { 
+        date : "13/01/2017",
+        totalBiscuits: 32, 
+        totalTeas: 34 
+    },
+    { 
+        date : "14/01/2017",
+        totalBiscuits: 32, 
+        totalTeas: 38 
+    },
+    { 
+        date : "15/01/2017",
+        totalBiscuits: 35, 
+        totalTeas: 41 
+    },
+    { 
+        date : "16/01/2017",
+        totalBiscuits: 38, 
+        totalTeas: 45
+    },
+    { 
+        date : "17/01/2017",
+        totalBiscuits: 40, 
+        totalTeas: 48 
+    },
+    { 
+        date : "18/01/2017",
+        totalBiscuits: 40, 
+        totalTeas: 49 
+    },
+    { 
+        date : "19/01/2017",
+        totalBiscuits: 40, 
+        totalTeas: 49 
+    },
+    { 
+        date : "20/01/2017",
+        totalBiscuits: 43, 
+        totalTeas: 51 
+    },
+    { 
+        date : "21/01/2017",
+        totalBiscuits: 47, 
+        totalTeas: 55 
+    },
+    { 
+        date : "22/01/2017",
+        totalBiscuits: 47, 
+        totalTeas: 58 
+    },
+    { 
+        date : "23/01/2017",
+        totalBiscuits: 49, 
+        totalTeas: 59
+    },
+    { 
+        date : "24/01/2017",
+        totalBiscuits: 49, 
+        totalTeas: 63 
+    },
+    { 
+        date : "25/01/2017",
+        totalBiscuits: 49, 
+        totalTeas: 66 
+    },
+    { 
+        date : "26/01/2017",
+        totalBiscuits: 49, 
+        totalTeas: 70 
+    },
+    { 
+        date : "27/01/2017",
+        totalBiscuits: 53, 
+        totalTeas: 75 
+    },
+    { 
+        date : "28/01/2017",
+        totalBiscuits: 55, 
+        totalTeas: 81 
+    },
+    { 
+        date : "29/01/2017",
+        totalBiscuits: 55, 
+        totalTeas: 84 
+    },
+    { 
+        date : "30/01/2017",
+        totalBiscuits: 55, 
+        totalTeas: 88 
+    },
+    { 
+        date :"31/01/2017", 
+        totalBiscuits:57, 
+        totalTeas:91 
+    } 
+] 
